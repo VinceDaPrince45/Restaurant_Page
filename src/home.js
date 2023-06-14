123c20769fd44533ce10a86369c2de9bc70acfd5
Vincent <Vincentnguyen030@gmail.com>
2023-06-13 23:34:36 -0500

    Manipulate dom to add tabs at top of page

--- src/home.js
+++ src/home.js
@@ -1,3 +1,30 @@
 export default function addHome() {
-    
+    let content = document.querySelector('#content');
+
+    // header
+    let header = document.createElement('div');
+    header.classList.add('header');
+
+    // tabs within header
+    let tabs = document.createElement('div');
+    tabs.classList.add('tabs');
+
+    let home = document.createElement('div');
+    home.textContent = 'Home';
+    home.classList.add('home');
+    home.classList.add('active');
+    let menu = document.createElement('div');
+    menu.textContent = 'Menu';
+    menu.classList.add('menu');
+    let contact = document.createElement('div');
+    contact.textContent = 'Contact';
+    contact.classList.add('contact');
+
+    tabs.appendChild(home);
+    tabs.appendChild(menu);
+    tabs.appendChild(contact);
+
+    header.appendChild(tabs);
+
+    // 
 }
--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,28 @@
 export default function addMenu() {
-    
+    let content = document.querySelector('#content');
+
+    // header
+    let header = document.createElement('div');
+    header.classList.add('header');
+
+    // tabs within header
+    let tabs = document.createElement('div');
+    tabs.classList.add('tabs');
+
+    let home = document.createElement('div');
+    home.textContent = 'Home';
+    home.classList.add('home');
+    let menu = document.createElement('div');
+    menu.textContent = 'Menu';
+    menu.classList.add('menu');
+    menu.classList.add('active');
+    let contact = document.createElement('div');
+    contact.textContent = 'Contact';
+    contact.classList.add('contact');
+
+    tabs.appendChild(home);
+    tabs.appendChild(menu);
+    tabs.appendChild(contact);
+
+    header.appendChild(tabs);
 }
