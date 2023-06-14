11e8982227c541bcc68ea66ad418db21bdd85d5a
Vincent <Vincentnguyen030@gmail.com>
2023-06-14 00:11:44 -0500

    Import function to load home page

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import './style.css';
+import addHome from './home.js';
+import addMenu from './menu.js';
+import addContact from './contact';
+
+const content = document.querySelector('#content');
+
+addHome();
+const home = document.querySelector('.home');
+home.onclick = () => {
+    content.textContent = '';
+    addHome();
+}
+const menu = document.querySelector('.menu');
+const contact = document.querySelector('.contact');
+
+
 console.log('hi');
