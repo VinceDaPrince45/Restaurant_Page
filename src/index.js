e95f58aa6a7c64cae1fb487fba59aae4657e5bf0
Vincent <Vincentnguyen030@gmail.com>
2023-06-14 18:16:28 -0500

    Add tabbing to main index

--- src/index.js
+++ src/index.js
@@ -5,14 +5,62 @@ import addContact from './contact';
 
 const content = document.querySelector('#content');
 
+// header
+let header = document.createElement('div');
+header.classList.add('header');
+
+// tabs within header
+let tabs = document.createElement('div');
+tabs.classList.add('tabs');
+
+let home = document.createElement('button');
+home.textContent = 'Home';
+home.classList.add('home');
+home.classList.add('active');
+let menu = document.createElement('button');
+menu.textContent = 'Menu';
+menu.classList.add('menu');
+let contact = document.createElement('button');
+contact.textContent = 'Contact';
+contact.classList.add('contact');
+
+tabs.appendChild(home);
+tabs.appendChild(menu);
+tabs.appendChild(contact);
+
+header.appendChild(tabs);
+content.append(header);
+
+// add content depending on tab
+
+// function to determine active tab
+function removeTabClass() {
+    home.classList.remove('active');
+    menu.classList.remove('active');
+    contact.classList.remove('active');
+};
+
 addHome();
-const home = document.querySelector('.home');
-home.onclick = () => {
-    content.textContent = '';
+home = document.querySelector('.home');
+home.addEventListener('click', () => {
+    content.removeChild(content.lastChild);
+    removeTabClass();
+    home.classList.add('active');
     addHome();
-}
-const menu = document.querySelector('.menu');
-const contact = document.querySelector('.contact');
+});
 
+menu = document.querySelector('.menu');
+menu.addEventListener('click', () => {
+    content.removeChild(content.lastChild);
+    removeTabClass();
+    menu.classList.add('active');
+    addMenu();
+});
 
-console.log('hi');
+contact = document.querySelector('.contact');
+contact.addEventListener('click', () => {
+    content.removeChild(content.lastChild);
+    removeTabClass();
+    contact.classList.add('active');
+    addContact();
+});
