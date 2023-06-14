2c69c5623a5d0d3539cd287b705b9d3c298c1f17
Vincent <Vincentnguyen030@gmail.com>
2023-06-14 18:16:38 -0500

    Create divs for contact page

--- src/contact.js
+++ src/contact.js
@@ -1,28 +1,40 @@
 export default function addContact() {
     let content = document.querySelector('#content');
 
-    // header
-    let header = document.createElement('div');
-    header.classList.add('header');
+    // body
+    let body = document.createElement('div');
+    body.classList.add('body');
 
-    // tabs within header
-    let tabs = document.createElement('div');
-    tabs.classList.add('tabs');
+    let container = document.createElement('div');
+    container.classList.add('container');
 
-    let home = document.createElement('div');
-    home.textContent = 'Home';
-    home.classList.add('home');
-    let menu = document.createElement('div');
-    menu.textContent = 'Menu';
-    menu.classList.add('menu');
-    let contact = document.createElement('div');
-    contact.textContent = 'Contact';
-    contact.classList.add('contact');
-    contact.classList.add('active');
+    // sections within container
+    const contactTitle = document.createElement('div');
+    contactTitle.textContent = 'Contact List';
 
-    tabs.appendChild(home);
-    tabs.appendChild(menu);
-    tabs.appendChild(contact);
+    const contactOne = document.createElement('div');
+    const contactOnePerson = document.createElement('div');
+    contactOnePerson.textContent = 'Vincent';
+    const contactOneDescription = document.createElement('div');
+    contactOneDescription.textContent = 'Owner and head chef: 111-111-1111';
+    contactOne.append(contactOnePerson,contactOneDescription);
 
-    header.appendChild(tabs);
+    const contactTwo = document.createElement('div');
+    const contactTwoPerson = document.createElement('div');
+    contactTwoPerson.textContent = 'Brent';
+    const contactTwoDescription = document.createElement('div');
+    contactTwoDescription.textContent = 'Sous chef: 111-111-1111';
+    contactTwo.append(contactTwoPerson,contactTwoDescription);
+
+    const contactThree = document.createElement('div');
+    const contactThreePerson = document.createElement('div');
+    contactThreePerson.textContent = 'Salman';
+    const contactThreeDescription = document.createElement('div');
+    contactThreeDescription.textContent = 'Manager: 111-111-1111';
+    contactThree.append(contactThreePerson,contactThreeDescription);
+
+    container.append(contactTitle,contactOne,contactTwo,contactThree);
+    body.append(container);
+
+    content.append(body);
 }
